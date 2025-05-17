284fc16eca16cd8debaf63bbe53b9b7beb147070
SK Maqshood <skmaqshood99@gmail.com>
2025-05-17 21:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
-    //Write your code here
+ var table = document.getElementById("sampleTable");
+	var newRow = table.insertRow(0);
+
+	var cell1 = newRow.insert(0);
+	var cell2 = newRow.imsert(1);
+
+	cell1.textcontent="New Cell1";
+	cell2.textcontent="New Cell2";
+	
   
   
 }
